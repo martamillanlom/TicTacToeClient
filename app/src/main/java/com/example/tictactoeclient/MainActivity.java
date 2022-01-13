97af9ac624c776ee6dfc932f01bba1aec97b1a30
Marta Millan Lombraña <marta.millan@iesjoandaustria.org>
2022-01-13 17:15:12 +0100

    primer commit

--- app/src/main/java/com/example/tictactoeclient/MainActivity.java
+++ app/src/main/java/com/example/tictactoeclient/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    }
+    } 
 
     public void updateUI(byte header){
         switch (header){
